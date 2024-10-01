7446c8eeb4de75f8ef90602202dda01939906e47
jongy <jongyong5645@gmail.com>
2024-10-01 15:38:38 +0900

    intial push

---
+++ src/test/java/com/example/chessdotnet/ChessDotNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessdotnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessDotNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
