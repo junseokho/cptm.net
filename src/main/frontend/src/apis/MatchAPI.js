46015895ce3de649bb9eb4ae0ef1fe64c4df3260
SONY-STRING <sctoa1@gmail.com>
2024-10-12 21:23:51 +0900

    [docs] jsdoc 파싱 더 잘되도록 함 : MatchAPI 의 class화 + 메소드는 static 메소드

--- src/main/frontend/src/apis/MatchAPI.js
+++ src/main/frontend/src/apis/MatchAPI.js
@@ -17,22 +17,25 @@ const axios_instanat = axios.create({
     }
 });
 
-/** 체스 대국들과 관련된 API 목록입니다. */
-export const MatchAPI = {
+/**
+ * @description 체스 대국들과 관련된 API 목록입니다.
+ * @author 손의현(SONY_STRING)
+ * */
+class MatchAPI {
     /**
      * @description 게임을 시작하지 않은 모든 대국 목록 정보를 가져옵니다.
      * @returns {Promise<axios.AxiosResponse<any>>} http 요청 결과
      */
-    get_available_matches: function () {
+    static get_available_matches () {
         return axios_instanat.get(
             `/api/rooms/available`,
         );
-    },
+    }
     /**
      * @description 새로운 대국을 생성합니다.
      * @returns {Promise<axios.AxiosResponse<any>>} http 요청 결과
      */
-    create_new_match: function (match_title, match_creator_id) {
+    static create_new_match (match_title, match_creator_id) {
         return axios_instanat.post(
             `/api/rooms/create`,
             {
@@ -40,17 +43,19 @@ export const MatchAPI = {
                 creatorId: match_creator_id
             },
         );
-    },
+    }
     /**
      * @description 대국에 참여합니다.
      * @returns {Promise<axios.AxiosResponse<any>>} http 요청 결과
      */
-    join_match: function (match_id, join_user_id) {
+    static join_match (match_id, join_user_id) {
         return axios_instanat.post(
             `/api/rooms/${match_id}/join`,
             {
                 userId: join_user_id
             }
         );
-    },
-};
+    }
+}
+
+export {MatchAPI};
