6b044757d5f342ee448d269d7db5ad0d7d99c1b5
SONY-STRING <sctoa1@gmail.com>
2024-12-15 00:12:47 +0900

    feat(createRoom): `RoomDTO` adapt to redesigned `Room` (#35)
    - 새롭게 바뀐 `Room` 에 맞추어 `RoomDTO` 를 수정하였습니다.
    - 이는 관련 API 의 response body 에 해당합니다.

--- src/main/java/com/example/chessdotnet/dto/RoomDTO.java
+++ src/main/java/com/example/chessdotnet/dto/RoomDTO.java
@@ -3,9 +3,10 @@ package com.example.chessdotnet.dto;
 import lombok.Data;
 
 /**
- * 방 정보를 전송하기 위한 데이터 전송 객체(DTO)입니다.
+ * `Room` 정보를 클라이언트에게 전송하기 위한 DTO 입니다.
  *
  * @author 전종영
+ * @apiNote 앞으로 필드가 추가될 수 있습니다.
  */
 @Data
 public class RoomDTO {
@@ -18,9 +19,25 @@ public class RoomDTO {
     /** 방 생성자의 이름 */
     private String hostUsername;
 
-    /** 현재 방에 참여 중인 플레이어 수 */
-    private int playersCount;
+    /** 방 생성자의 레이팅 */
+    private Integer hostRating;
 
-    /** 관전자 참여가능 여부 */
-    private boolean canJoinAsSpectator;
+
+    /** 플레이어로 참가 가능 여부 */
+    private Boolean canJoinAsPlayer;
+
+    /** 관전 가능 여부 */
+    private Boolean canJoinAsSpectator;
+
+    /** 게임 종료 여부 */
+    private Boolean isGameDone;
+
+    /** 타임 컨트롤 - 분 */
+    private Integer timeControlMin;
+
+    /** 타임 컨트롤 - 초 */
+    private Integer timeControlSec;
+
+    /** 타임 컨트롤 - 증초 */
+    private Integer timeControlInc;
 }
