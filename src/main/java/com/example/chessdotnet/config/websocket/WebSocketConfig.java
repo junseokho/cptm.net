96c22bd4e743e97943d66eaa4148d81b3b91065d
JEON, JONGYOUNG <74577670+jongyoung3@users.noreply.github.com>
2024-12-16 11:00:38 +0900

    feat(websocket): 관전자 큐 설정 추가 (#59)
    - WebSocket 메시지 브로커 설정에 관전자를 위한 큐 엔드포인트를 추가했습니다.
    - 이를 통해 관전자들에게 게임 상태 업데이트를 효과적으로 전달할 수 있게 되었습니다.

--- src/main/java/com/example/chessdotnet/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/chessdotnet/config/websocket/WebSocketConfig.java
@@ -44,7 +44,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/queue/spectator");
         config.setApplicationDestinationPrefixes("/app");
     }
 
